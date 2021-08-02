26f421a81b4b7e95b23ede5dcd3a46ac7648fb20
Nina Davis <nina.k.davis2@gmail.com>
2021-08-01 19:28:04 -0500

    handlebars

--- server.js
+++ server.js
@@ -1,20 +1,18 @@
+const path = require('path');
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./constrollers');
 const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
-const express = require('express');
-const routes = require('./routes');
-const sequelize = require('./config/connection');
-
-const app = express();
-const PORT = process.env.PORT || 3001;
-
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.static(path.join(__dirname, 'public')));
 // turn on routes
 app.use(routes);
 
